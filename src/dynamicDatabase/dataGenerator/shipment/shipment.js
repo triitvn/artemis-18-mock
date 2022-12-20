29a599ccb46690de2434a72b41a4ccacc40be016
Tri <tri@parcelperform.com>
2022-12-20 11:29:54 +0700

    fix: update shipment status

--- src/dynamicDatabase/dataGenerator/shipment/shipment.js
+++ src/dynamicDatabase/dataGenerator/shipment/shipment.js
@@ -5,7 +5,7 @@ const generateParcel = require("./parcel");
 const generateShipment = () => {
   const shipment = {
     id: Number.parseInt(faker.datatype.number({ max: 99999 }), 10),
-    shipment_status: "inactive",
+    shipment_status: "active",
     shipment_id: "SHIP7101192",
     line_items: generateLineItems(3),
     parcel: {
